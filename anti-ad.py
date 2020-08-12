346a90999d1cab67dd12d5ac4781221a59279a34
Lee <21115521+yancylee99@users.noreply.github.com>
2020-08-12 08:50:56 +0800

    Add files via upload

---
+++ anti-ad.py
@@ -0,0 +1,19 @@
+import requests
+import os
+try:
+    os.remove('anti-ad.yaml')
+except Exception as e:
+    print(e)
+url = 'https://anti-ad.net/surge.txt' 
+r = requests.get(url)
+with open("anti-ad.yaml",'w') as f:
+    f.write('payload:\n')
+    for line in r.text.split('\n'):
+        try:
+            if(line[0]=='D'):
+                f.write('  - '+line+'\n')
+            else:
+                f.write(line+'\n')
+        except Exception as e:
+            print(e)
+        
